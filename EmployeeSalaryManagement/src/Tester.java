e397f403e3fba0b9721dea2896ca2c5f40be0b32
Iuliia Bazhutina <Iuliia.Bazhutina@gmail.com>
2025-03-30 22:34:43 -0400

    created employee and test classes

---
+++ EmployeeSalaryManagement/src/Tester.java
@@ -0,0 +1,17 @@
+
+public class Tester {
+
+public Tester(){
+Employee employee1 = new Employee("john", "smith", "sales representative");
+Employee employee2 = new Employee("KATE", "MCDONALD", "");
+
+    System.out.println(employee1);
+    System.out.println(employee2);
+}
+
+
+    public static void main(String[] args) {
+       Tester test = new Tester();
+    }
+    
+}
